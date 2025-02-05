8801a876d1e210b4c465b9b706f979d96002726f
yurdhjiokk <niteenbg848@gmail.com>
2025-02-05 18:43:31 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "NARUTO Bot",
+  botOwner: 'NARUTO Bot', // Your name
+  ownerNumber: '+919035101763', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
